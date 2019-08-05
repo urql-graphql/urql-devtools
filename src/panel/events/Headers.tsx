6b94df631889334177d48c255a7d3ea4acceac32
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-05 11:32:12 +0100

    (feat) - hide headers on small devtools panel

--- src/panel/events/Headers.tsx
+++ src/panel/events/Headers.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { smMax, mdMin } from "./constants";
+import { mdMin } from "./constants";
 
 export const Headers = () => (
   <Container>
@@ -13,57 +13,34 @@ export const Headers = () => (
 
 const BaseTitle = styled.h3`
   color: rgba(255, 255, 255);
+  font-size: 13px;
   margin: 0;
   width: 50%;
-
-  @media (min-width: ${mdMin}) {
-    font-size: 13px;
-  }
 `;
 
 const OperationName = styled(BaseTitle)`
-  @media (max-width: ${smMax}) {
-    margin-bottom: 10px;
-  }
-
-  @media (min-width: ${mdMin}) {
-    flex-basis: 4;
-    order: 1;
-    width: 20%;
-  }
+  flex-basis: 4;
+  order: 1;
+  width: 20%;
 `;
 
 const OperationTime = styled(BaseTitle)`
-  text-align: right;
-
-  @media (min-width: ${mdMin}) {
-    font-size: 13px;
-    order: 4;
-    width: 20%;
-  }
+  font-size: 13px;
+  order: 4;
+  width: 20%;
 `;
 
 const OperationAddInfo = styled(BaseTitle)`
-  @media (min-width: ${mdMin}) {
-    font-size: 13px;
-    order: 2;
-    width: 25%;
-  }
+  font-size: 13px;
+  order: 2;
+  width: 25%;
 `;
 
 const OperationKey = styled(BaseTitle)`
-  text-align: right;
-
-  @media (max-width: ${smMax}) {
-    width: 50%;
-  }
-
-  @media (min-width: ${mdMin}) {
-    font-size: 13px;
-    order: 3;
-    text-align: left;
-    width: 25%;
-  }
+  font-size: 13px;
+  order: 3;
+  text-align: left;
+  width: 25%;
 `;
 
 const Container = styled.div`
@@ -71,17 +48,13 @@ const Container = styled.div`
   background-color: ${props => props.theme.dark[0]};
   width: auto;
   height: auto;
-  display: flex;
-  flex-direction: row;
   padding: 10px 15px;
-
-  @media (max-width: ${smMax}) {
-    flex-wrap: wrap;
-    align-items: baseline;
-  }
+  display: none;
 
   @media (min-width: ${mdMin}) {
     align-items: center;
+    display: flex;
+    flex-direction: row;
     justify-content: space-between;
   }
 `;
