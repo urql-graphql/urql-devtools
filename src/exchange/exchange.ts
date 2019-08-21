315fcb393712ea948c9758fdffaeed0f2ba5c603
Phil Pluckthun <phil@kitten.sh>
2019-08-21 12:14:55 +0100

    Copy over metadata changes from urql-devtools-exchange

--- src/exchange/exchange.ts
+++ src/exchange/exchange.ts
@@ -1,4 +1,4 @@
-import { pipe, tap, toPromise } from "wonka";
+import { map, pipe, tap, toPromise } from "wonka";
 import {
   Exchange,
   Client,
@@ -6,7 +6,13 @@ import {
   OperationResult,
   createRequest
 } from "urql";
-import { UrqlEvent, DevtoolsMessage } from "../types";
+import {
+  DevtoolsExchangeOutgoingMessage,
+  DevtoolsExchangeOutgoingEventType,
+  ExecuteRequestMessage,
+  DevtoolsExchangeIncomingEventType
+} from "./types";
+import { getDisplayName } from "./getDisplayName";
 
 export const devtoolsExchange: Exchange = ({ client, forward }) => {
   if (process.env.NODE_ENV === "production") {
@@ -17,72 +23,114 @@ export const devtoolsExchange: Exchange = ({ client, forward }) => {
       );
   }
 
-  window.__urql__ = {
-    client,
-    events: []
-  };
+  // Expose graphql url for introspection
+  window.__urql__.url = client.url;
 
-  // Initialize
-  sendToContentScript("init");
-
-  // Listen for messages
-  window.addEventListener("urql-in", e =>
-    handleMessage(client)((e as CustomEvent).detail)
+  // Listen for messages from content script
+  window.addEventListener(
+    DevtoolsExchangeIncomingEventType,
+    (e: CustomEvent) => {
+      const handler = messageHandlers[e.detail.type];
+      handler && handler(client)(e.detail);
+    }
   );
+  sendToContentScript({ type: "init" });
 
   return ops$ => {
     return pipe(
       ops$,
+      map(addOperationContext),
       tap(handleOperation),
       forward,
+      map(addOperationResponseContext),
       tap(handleOperation)
     );
+  };
+};
+
+const addOperationResponseContext = (op: OperationResult): OperationResult => {
+  return {
+    ...op,
+    operation: {
+      ...op.operation,
+      context: {
+        ...op.operation.context,
+        meta: {
+          ...op.operation.context.meta,
+          // @ts-ignore
+          networkLatency: Date.now() - op.operation.context.meta.startTime
+        }
+      }
+    }
+  };
+};
+
+const addOperationContext = (op: Operation): Operation => {
+  return {
+    ...op,
+    context: {
+      ...op.context,
+      meta: {
+        ...op.context.meta,
+        source: getDisplayName(),
+        // @ts-ignore
+        startTime: Date.now()
+      }
+    }
   };
 };
 
 /** Handle operation or response from stream. */
 const handleOperation = <T extends Operation | OperationResult>(op: T) => {
   const event = JSON.parse(JSON.stringify(parseStreamData(op))); // Serialization required for some events (such as error)
-
-  // Dispatch for panel
   sendToContentScript(event);
-
-  // Add to window cache
-  window.__urql__.events = [event, ...window.__urql__.events];
 };
 
-const handleMessage = (client: Client) => (message: DevtoolsMessage) => {
-  if (message.type === "request") {
-    const isMutation = /(^|\W)+mutation\W/.test(message.query);
-    const execFn = isMutation ? client.executeMutation : client.executeQuery;
+/** Handles execute request messages. */
+const requestHandler = (client: Client) => (message: ExecuteRequestMessage) => {
+  const isMutation = /(^|\W)+mutation\W/.test(message.query);
+  const execFn = isMutation ? client.executeMutation : client.executeQuery;
 
-    pipe(
-      execFn(createRequest(message.query), {
-        meta: { source: "Devtools" }
-      }),
-      toPromise
-    );
-  }
+  pipe(
+    execFn(createRequest(message.query), {
+      meta: { source: "Devtools" }
+    }),
+    toPromise
+  );
 };
 
+/** Map of handlers for incoming messages. */
+const messageHandlers = {
+  request: requestHandler
+} as const;
+
+/** Creates a DevtoolsExchangeOutgoingMessage from operations/responses. */
 const parseStreamData = <T extends Operation | OperationResult>(op: T) => {
   const timestamp = new Date().valueOf();
 
   // Outgoing operation
   if ("operationName" in op) {
-    return { type: "operation", data: op as Operation, timestamp } as const;
+    return {
+      type: "operation",
+      data: op,
+      timestamp
+    } as const;
   }
 
   // Incoming error
   if ((op as OperationResult).error !== undefined) {
-    return { type: "error", data: op as OperationResult, timestamp } as const;
+    return { type: "error", data: op, timestamp } as const;
   }
 
   // Incoming response
-  return { type: "response", data: op as OperationResult, timestamp } as const;
+  return {
+    type: "response",
+    data: op,
+    timestamp
+  } as const;
 };
 
-const sendToContentScript = (oe: ContentScriptEvent) =>
-  window.dispatchEvent(new CustomEvent("urql-out", { detail: oe }));
-
-type ContentScriptEvent = UrqlEvent | "init";
+const sendToContentScript = (detail: DevtoolsExchangeOutgoingMessage) =>
+  window.dispatchEvent(
+    new CustomEvent(DevtoolsExchangeOutgoingEventType, { detail })
+  );
