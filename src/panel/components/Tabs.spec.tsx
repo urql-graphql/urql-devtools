aa57d43e3619cb76d6479c3b5e96665830712291
Andy Richardson <andy.john.richardson@gmail.com>
2019-11-14 16:36:30 +0000

    setup testing infra

---
+++ src/panel/components/Tabs.spec.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { Tabs } from "./Tabs";
+
+beforeEach(jest.clearAllMocks);
+
+const props = {
+  active: "a",
+  options: [
+    { label: "first", value: "a" },
+    { label: "second", value: "b" }
+  ],
+  setActive: jest.fn()
+};
+
+describe("on mount", () => {
+  it("matches snapshot", () => {
+    const wrapper = shallow(<Tabs {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe("on tab click", () => {
+  it("calls setActive", () => {
+    const wrapper = shallow(<Tabs {...props} />);
+    wrapper.find("Tab:last-child").simulate("click");
+    expect(props.setActive).toBeCalledWith("b");
+  });
+});
