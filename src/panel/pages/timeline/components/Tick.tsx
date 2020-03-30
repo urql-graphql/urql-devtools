b9b40c2f9246fe0bb143903ed19fc0d4ae0e435f
Andy Richardson <andy.john.richardson@gmail.com>
2020-03-30 17:02:22 +0100

    Update time ticks to match designs

--- src/panel/pages/timeline/components/Tick.tsx
+++ src/panel/pages/timeline/components/Tick.tsx
@@ -2,15 +2,31 @@ import styled from "styled-components";
 
 export const Tick = styled.div<{ label: string }>`
   position: absolute;
-  background: ${(props) => props.theme.dark["+1"]};
   width: 2px;
-  top: 0;
+  top: 20px;
   bottom: 0;
 
   &:before {
     content: "${(props) => props.label}";
-    margin-left: 10px;
-    font-size: 10px;
-    color: ${(props) => props.theme.grey["0"]};
+    font-family: "Roboto";
+    font-size: 12px;
+    color: #fff;
+    opacity: 0.9;
+
+    display: block;
+    text-align: center;
+    width: 100px;
+    margin-top: -25px;
+    margin-left: -50px;
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    width: 2px;
+    top: 0;
+    bottom: 0;
+    background: ${(props) => props.theme.dark["+1"]};
+    opacity: 0.3;
   }
 `;
