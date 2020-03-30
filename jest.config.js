ca7cb658cdace8e16f91e8b657c400d0fe7b196c
Andy Richardson <andy.john.richardson@gmail.com>
2020-03-30 16:33:02 +0100

    Make jest pretty

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   transform: {
-    "^.+\\.tsx?$": "babel-jest"
+    "^.+\\.tsx?$": "babel-jest",
   },
   setupFiles: ["dotenv/config"],
   setupFilesAfterEnv: ["<rootDir>src/setupTests.ts"],
@@ -9,7 +9,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.svg$": "<rootDir>/src/panel/__mocks__/svg.tsx",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
-      "identity-obj-proxy"
+      "identity-obj-proxy",
   },
-  testEnvironment: "jsdom"
+  testEnvironment: "jsdom",
 };
