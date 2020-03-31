4e380c827ed6ce9c7abe9a1d43790b3f39bb771d
Will Golledge <35961363+wgolledge@users.noreply.github.com>
2020-03-31 11:08:34 +0100

    Add OperationType icons to the timeline (#130)
    * Add TimelineIcons column to the timeline - showing an icon for the row's associated operationName

--- src/panel/pages/timeline/Timeline.tsx
+++ src/panel/pages/timeline/Timeline.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useTimelineContext } from "../../context";
 import { Background } from "../../components/Background";
 import { TimelineRow, TimelinePane, Tick } from "./components";
+import { TimelineIcon } from "./components/TimelineIcon";
 
 export const Timeline: FC = () => {
   const {
@@ -32,41 +33,82 @@ export const Timeline: FC = () => {
     [scale]
   );
 
+  const operations = useMemo(
+    () =>
+      Object.entries(events).map(([key, eventList]) => ({
+        key,
+        operationName: eventList[0].operation.operationName,
+      })),
+    [events]
+  );
+
   // We lie about the types to save having to do this check
   // in every component. This guard is needed.
   if (!container)
     return (
       <Page>
-        <TimelineList ref={setContainer} />
+        <TimelineContainer>
+          {/* Key is needed to retain the order for the ref */}
+          <TimelineList ref={setContainer} key="TimelineList" />
+        </TimelineContainer>
       </Page>
     );
 
   return (
     <Page>
-      <TimelineList ref={setContainer} draggable="true">
-        {ticks.map((t, i) => (
-          <Tick key={`p-${i}`} label={t.label} style={{ left: t.position }} />
-        ))}
-        {Object.entries(events).map(([key, eventList]) => (
-          <TimelineRow key={key} events={eventList} />
-        ))}
-      </TimelineList>
+      <TimelineContainer>
+        <TimelineIcons>
+          {operations.map((op) => (
+            <TimelineIcon key={op.key} operation={op.operationName} />
+          ))}
+        </TimelineIcons>
+        <TimelineList ref={setContainer} draggable="true" key="TimelineList">
+          {ticks.map((t, i) => (
+            <Tick key={`p-${i}`} label={t.label} style={{ left: t.position }} />
+          ))}
+          {Object.entries(events).map(([key, eventList]) => (
+            <TimelineRow key={key} events={eventList} />
+          ))}
+        </TimelineList>
+      </TimelineContainer>
       {selectedEvent && <TimelinePane event={selectedEvent} />}
     </Page>
   );
 };
 
+const SPACING = 40;
+const ROW_PADDING = 8;
+
 const Page = styled(Background)`
   background-color: ${(p) => p.theme.dark["0"]};
+`;
+
+const TimelineContainer = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
+
+const TimelineIcons = styled.div`
+  background-color: ${(p) => p.theme.dark["-3"]};
+  display: grid;
+  justify-content: center;
+  grid-template-rows: 20px;
+  grid-row-gap: ${ROW_PADDING * 2}px;
+  /* TimelineList outer margin + inner margin + padding */
+  padding: ${SPACING + ROW_PADDING + 30}px 0;
+  position: relative;
+  width: ${SPACING}px;
+  z-index: 1;
 `;
 
 const TimelineList = styled.div`
   cursor: grab;
   display: flex;
-  flex-grow: 1;
+  width: calc(100% - ${SPACING}px);
   flex-direction: column;
+  flex-grow: 1;
   position: relative;
-  margin: 40px 0;
+  margin: ${SPACING}px 0;
   &:active {
     cursor: grabbing;
   }
