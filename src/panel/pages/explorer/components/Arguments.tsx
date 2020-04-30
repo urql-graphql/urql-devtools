b4ee4bcd9e9aae8bdf32ab73bd2c54396f6b516c
gsporie <george.sporieiii@poly.com>
2020-04-30 10:38:54 -0600

    Fix crash on undefined argument value.

--- src/panel/pages/explorer/components/Arguments.tsx
+++ src/panel/pages/explorer/components/Arguments.tsx
@@ -9,19 +9,18 @@ export const Arguments: FC<{
   if (!args) {
     return null;
   }
-
   const entries = useMemo(() => Object.entries(args), [args]);
-
   return (
     <ArgumentText>
       (
-      {entries.map(([key, value], index) => (
-        <Fragment key={key}>
-          {`${key}: `}
-          <InlineCodeHighlight code={JSON.stringify(value)} language="json" />
-          {index !== entries.length - 1 && ", "}
-        </Fragment>
-      ))}
+      {entries
+        .map(([key, value], index) => (
+            <Fragment key={key}>
+              {`${key}: `}
+              <InlineCodeHighlight code={JSON.stringify(value) || "undefined"} language="json"/>
+              {index !== entries.length - 1 && ", "}
+            </Fragment>
+          ))}
       )
     </ArgumentText>
   );
