0cbf1c742fdb7893402681ddda8e3ed5d7e58ade
Andy Richardson <andy.john.richardson@gmail.com>
2020-05-05 16:38:38 +0100

    Explicitly specify targets

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
+    [
+      "@babel/preset-env",
+      { targets: { firefox: "70", chrome: "78", electron: "8", node: "12" } },
+    ],
     "@babel/preset-react",
     "@babel/preset-typescript",
   ],
