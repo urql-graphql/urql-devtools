bce30a0b4ce1300f6431aad285a60a08e19dda76
Andy Richardson <andy.john.richardson@gmail.com>
2020-05-05 11:11:31 +0100

    Use babel to transpile optional chaining

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = {
   ],
   plugins: [
     "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-optional-chaining",
     [
       "babel-plugin-styled-components",
       {
