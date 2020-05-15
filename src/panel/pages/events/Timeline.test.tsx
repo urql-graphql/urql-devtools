1b77f2a6f22d2e0d3717e0f4eec3fd31db06c7d6
Andy Richardson <andy.john.richardson@gmail.com>
2020-05-15 15:24:23 +0100

    Fix refs not being forwarded and add Timeline test

---
+++ src/panel/pages/events/Timeline.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
+import Decorator from "../../cosmos.decorator";
+import fixtures from "./Timeline.fixture";
+jest.mock("../../hooks", () => ({
+  useOrientationWatcher: () => ({ isPortrait: true, isLandscape: false }),
+}));
+
+beforeAll(() => {
+  jest.useFakeTimers();
+});
+
+describe("after all events have been dispatched", () => {
+  it("matches snapshot", () => {
+    const wrapper = mount(<Decorator>{fixtures.dynamic}</Decorator>);
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    console.log(wrapper.debug());
+  });
+});
