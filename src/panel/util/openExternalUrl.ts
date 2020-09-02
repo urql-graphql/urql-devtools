3c773cf001ff8e66b61236d74a8d8201baebced2
Alexander Keliris <rigellute@gmail.com>
2020-09-02 12:57:58 +0100

    Fix electron require
    Turns out that using `import` from electron causes the error
    
    > fs.existsSync is not a function
    
    when running `yarn cosmos`.
    
    Fix is to use `require` instead of `import`.

--- src/panel/util/openExternalUrl.ts
+++ src/panel/util/openExternalUrl.ts
@@ -1,9 +1,8 @@
-import { shell } from "electron";
-
 export const openExternalUrl = (url: string): Window | null | undefined => {
   if (process.env.BUILD_ENV === "extension") {
     return window.open(url, "_blank");
   }
 
+  const shell = require("electron").shell as import("electron").Shell;
   shell.openExternal(url);
 };
