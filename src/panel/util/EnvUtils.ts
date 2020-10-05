9790be577a748f9bdac13bdbfa8b50f95f97f8ab
Brandon Konkle <brandon@konkle.us>
2020-10-05 08:47:44 -0600

    Support Electron theming in a backwards-compatible way

--- src/panel/util/EnvUtils.ts
+++ src/panel/util/EnvUtils.ts
@@ -1,12 +1,22 @@
 export const isDarkMode = (): boolean => {
+  // Browser extensions
   if (process.env.BUILD_ENV === "extension") {
     return chrome.devtools.panels.themeName === "dark";
   }
 
+  // Modern Electron
+  const nativeTheme = require("electron")
+    .nativeTheme as import("electron").NativeTheme;
+
+  if (nativeTheme) {
+    return nativeTheme.shouldUseDarkColors;
+  }
+
+  // Legacy Electron
   const systemPreferences = require("electron")
     .systemPreferences as import("electron").SystemPreferences;
 
-  return systemPreferences.isDarkMode();
+  return systemPreferences?.isDarkMode() || true;
 };
 
 export const isLightMode = (): boolean => !isDarkMode();
