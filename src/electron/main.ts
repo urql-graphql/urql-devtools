3c7cc7b9d3c9e5b9062e600d195e57b0aed23a80
Manos Konstantinidis <hello@manos.im>
2021-07-05 13:46:15 +0100

    chore: Fix prettier linting

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -49,7 +49,7 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
+      contextIsolation: false,
     },
   });
   windows = [...windows, win];
