20efb00286e36039decde1af9984e02f38e65e75
Andy Richardson <andy.john.richardson@gmail.com>
2021-12-03 10:37:34 +0000

    Fix prettier errors

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 const { spawn } = require("child_process");
-spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ["start"], { stdio: "inherit", cwd: `${__dirname}/..` });
+spawn(/^win/.test(process.platform) ? "npm.cmd" : "npm", ["start"], {
+  stdio: "inherit",
+  cwd: `${__dirname}/..`,
+});
