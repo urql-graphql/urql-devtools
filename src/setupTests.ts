0cfa41876a07441e6b5f153d7d486a95c4e314f5
jdecroock <decroockjovi@gmail.com>
2022-11-25 11:20:09 +0100

    enzyme react 17

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
 import "jest-styled-components";
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import puppeteer from "puppeteer";
 
 process.env.BUILD_ENV = "extension";
