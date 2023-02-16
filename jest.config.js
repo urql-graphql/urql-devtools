c64a4214ece79c09cabc1be236b4bb0001243d82
jdecroock <decroockjovi@gmail.com>
2023-02-16 10:01:39 +0100

    transform cjs differently

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "^.+\\.[t|j]sx?$": "babel-jest",
     "^.+\\.js$": "babel-jest",
   },
-  transformIgnorePatterns: ["node_modules/(?!(nanoid)/)"],
+  transformIgnorePatterns: ["node_modules/.pnpm/(?!nanoid)"],
   setupFiles: ["dotenv/config"],
   setupFilesAfterEnv: ["<rootDir>src/setupTests.ts"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
