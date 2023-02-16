4d4830b233be2a2f2404c245199fc923ff5a0bce
jdecroock <decroockjovi@gmail.com>
2023-02-16 09:44:35 +0100

    run prettier

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,33 +1,33 @@
-const { getInfo } = require('@changesets/get-github-info');
+const { getInfo } = require("@changesets/get-github-info");
 
-const REPO = 'urql-graphql/urql-devtools';
+const REPO = "urql-graphql/urql-devtools";
 const SEE_LINE = /^See:\s*(.*)/i;
 const TRAILING_CHAR = /[.;:]$/g;
-const listFormatter = new Intl.ListFormat('en-US');
+const listFormatter = new Intl.ListFormat("en-US");
 
-const getSummaryLines = cs => {
+const getSummaryLines = (cs) => {
   const lines = cs.summary
     .trim()
     .split(/[\r\n]+/)
-    .map(l => l.trim())
+    .map((l) => l.trim())
     .filter(Boolean);
   const size = lines.length;
   if (size > 0) {
-    lines[size - 1] = lines[size - 1].replace(TRAILING_CHAR, '');
+    lines[size - 1] = lines[size - 1].replace(TRAILING_CHAR, "");
   }
 
   return lines;
 };
 
 /** Creates a "(See X)" string from a template */
-const templateSeeRef = links => {
-  const humanReadableLinks = links.filter(Boolean).map(link => {
-    if (typeof link === 'string') return link;
+const templateSeeRef = (links) => {
+  const humanReadableLinks = links.filter(Boolean).map((link) => {
+    if (typeof link === "string") return link;
     return link.pull || link.commit;
   });
 
   const size = humanReadableLinks.length;
-  if (size === 0) return '';
+  if (size === 0) return "";
 
   const str = listFormatter.format(humanReadableLinks);
   return `(See ${str})`;
@@ -35,14 +35,14 @@ const templateSeeRef = links => {
 
 const changelogFunctions = {
   getDependencyReleaseLine: async (changesets, dependenciesUpdated) => {
-    if (dependenciesUpdated.length === 0) return '';
+    if (dependenciesUpdated.length === 0) return "";
 
     const dependenciesLinks = await Promise.all(
-      changesets.map(async cs => {
+      changesets.map(async (cs) => {
         if (!cs.commit) return undefined;
 
         const lines = getSummaryLines(cs);
-        const prLine = lines.find(line => SEE_LINE.test(line));
+        const prLine = lines.find((line) => SEE_LINE.test(line));
         if (prLine) {
           const match = prLine.match(SEE_LINE);
           return (match && match[1].trim()) || undefined;
@@ -57,22 +57,22 @@ const changelogFunctions = {
       })
     );
 
-    let changesetLink = '- Updated dependencies';
+    let changesetLink = "- Updated dependencies";
 
     const seeRef = templateSeeRef(dependenciesLinks);
     if (seeRef) changesetLink += ` ${seeRef}`;
 
-    const detailsLinks = dependenciesUpdated.map(dep => {
+    const detailsLinks = dependenciesUpdated.map((dep) => {
       return `  - ${dep.name}@${dep.newVersion}`;
     });
 
-    return [changesetLink, ...detailsLinks].join('\n');
+    return [changesetLink, ...detailsLinks].join("\n");
   },
   getReleaseLine: async (changeset, type) => {
     let pull, commit, user;
 
     const lines = getSummaryLines(changeset);
-    const prLineIndex = lines.findIndex(line => SEE_LINE.test(line));
+    const prLineIndex = lines.findIndex((line) => SEE_LINE.test(line));
     if (prLineIndex > -1) {
       const match = lines[prLineIndex].match(SEE_LINE);
       pull = (match && match[1].trim()) || undefined;
@@ -92,14 +92,14 @@ const changelogFunctions = {
       user = links.user || undefined;
     }
 
-    let annotation = '';
-    if (type === 'patch' && /^\s*fix/i.test(firstLine)) {
-      annotation = '⚠️ ';
+    let annotation = "";
+    if (type === "patch" && /^\s*fix/i.test(firstLine)) {
+      annotation = "⚠️ ";
     }
 
     let str = `- ${annotation}${firstLine}`;
     if (futureLines.length > 0) {
-      str += `\n${futureLines.map(l => `  ${l}`).join('\n')}`;
+      str += `\n${futureLines.map((l) => `  ${l}`).join("\n")}`;
     }
 
     if (user) {
